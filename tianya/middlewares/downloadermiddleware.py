d0ab5403542b005f40d988f58154983c75686759
liuyang <place222@hotmail.com>
2016-02-26 17:31:38 +0800

    first day

---
+++ tianya/middlewares/downloadermiddleware.py
@@ -0,0 +1,14 @@
+# -*-coding=utf-8-*-
+"""
+
+"""
+class MyDownLoaderMiddleware(object):
+    """
+
+    """
+    # def process_request(self,request,spider):
+    #     pass
+    # def process_response(self,response,spider):
+    #     pass
+    # def process_exception(self,request,exception,spider):
+    #     pass
